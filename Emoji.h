fabdf0e12a8f404898386bdcf71b814611551a10
Cebotari Roman <cebotari.roman.works@gmail.com>
2014-08-12 12:52:22 +0300

    Emoji class

---
+++ Emoji.h
@@ -0,0 +1,19 @@
+//
+//  Emoji.h
+//  ChatRoulette
+//
+//  Created by Roman Cebotari on 8/12/14.
+//  Copyright (c) 2014 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Emoji : NSObject
+
++ (NSArray *)peopleEmoji;
++ (NSArray *)natureEmoji;
++ (NSArray *)objectEmoji;
++ (NSArray *)placesEmoji;
++ (NSArray *)symbolEmoji;
+
+@end
